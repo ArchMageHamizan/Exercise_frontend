74a487d7a7c416a1dd00ed13ac7f999d83cc5028
Hamizan <hamizanyus000@gmail.com>
2024-03-03 21:50:34 +0800

    update_1: from Hamizan Chart clarification method using

---
+++ README_FROM_HAMIZAN.txt
@@ -0,0 +1,5 @@
+1/ npm install -g @vue/cli
+2/ npm install @syncfusion/ej2-vue-charts --save
+   2.1 import the chart plugins into App.vue @ your vue file.
+   2.2 register chartPlugin in thay vue file
+   2.3 import registerLicense  into main.js
---
+++ src/components/Chart.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+
+   import  Chart  from "chart.js/auto";
+   import { onMounted } from "vue";
+//    const setup = () => {
+//         window.addEventListener('DOMContentLoaded',()=>{
+//             const ctx = document.getElementById('myChart');
+//             if(ctx instanceof HTMLCanvasElement){
+//                 const myChart=new Chart(ctx, {
+//                                 type: 'bar',
+//                                 data: {
+//                                 labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//                                 datasets: [{
+//                                     label: '# of Votes',
+//                                     data: [12, 19, 3, 5, 2, 3],
+//                                     borderWidth: 1
+//                                 }]
+//                                 },
+//                                 options: {
+//                                 scales: {
+//                                     y: {
+//                                     beginAtZero: true
+//                                     }
+//                                 }
+//                                 }
+//                         });
+//                         }
+//         });
+//     }
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+    ];
+
+    const data = {
+        labels: labels,
+        datasets: [{
+        label: 'My First dataset',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    };
+
+    const config = {
+        type: 'line',
+        data: data,
+        options: {}
+    };
+    onMounted(()=>{
+        const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+        );
+    })   
+        
+
+        
+  
+</script>
+<template>
+    <h3 style="text-align: center;">Hello Hamizan</h3>
+    <br>
+    <div>
+        <canvas id="myChart"></canvas>
+    </div>
+ 
+</template>
